bf69fc694efd4d3755fd03ddd567496216879eea
RuslanaSydorchuk <104679795+RuslanaSydorchuk@users.noreply.github.com>
2022-12-28 00:25:46 +0200

    виконала правки

--- src/components/FriendList/FriendListItems.jsx
+++ src/components/FriendList/FriendListItems.jsx
@@ -2,13 +2,11 @@ import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
 
 export default function FriendListItem({friend}){
-    let bgColor = 'red';
-    if (friend.isOnline) {
-    bgColor = 'green';}
+
     return(
         
         <>
-            <span className = {css.status} style={{ backgroundColor: bgColor}}>{friend.isOnline}</span>
+            <span className={`${css.status} ${friend.isOnline ? css.online : css.offline}`} ></span>
             <img className={css.avatar} src= {friend.avatar} alt= {friend.name} width="48" />
             <p className={css.name}>{friend.name}</p>
         </>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,35 +3,17 @@ import css from './Statistic.module.css';
 
 export default function Statistic ({title, stats}){
 
-    let isTitle = false;
-    if (title !== '') {
-        isTitle = true;
-    } else{
-        isTitle = false;
-    };
-
-    const sectionTitle = <h2 className={css.title}>Upload stats</h2>
     return(
         <section className={css.statistics}>
-            {isTitle ? sectionTitle : null}
+            {title && <h2 className={css.title}>{title}</h2>}
 
           <ul className={css.statList}>
-            <li className={css.item} key = {stats[0].id} style = {{backgroundColor: getRandomHexColor()}}>
-              <span className={css.label}>{stats[0].label}</span>
-              <span className={css.percentage}>{stats[0].percentage}%</span>
-            </li>
-            <li className={css.item} key = {stats[1].id} style = {{backgroundColor: getRandomHexColor()}}>
-              <span className={css.label}>{stats[1].label}</span>
-              <span className={css.percentage}>{stats[1].percentage}%</span>
-            </li>
-            <li className={css.item} key = {stats[2].id} style = {{backgroundColor: getRandomHexColor()}}>
-              <span className={css.label}>{stats[2].label}</span>
-              <span className={css.percentage}>{stats[2].percentage}%</span>
-            </li>
-            <li className={css.item} key = {stats[3].id} style = {{backgroundColor: getRandomHexColor()}}>
-              <span className={css.label}>{stats[3].label}</span>
-              <span className={css.percentage}>{stats[3].percentage}%</span>
-            </li>
+            {stats.map(({id, label, percentage})=>
+              <li className={css.item} key = {id} style = {{backgroundColor: getRandomHexColor()}}>
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage}%</span>
+              </li>
+            )}
           </ul>
         </section>
     )
